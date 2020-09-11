c2ce35c4e4157694795bf382e2d16f42e2197eef
neriksen <nielseriksen@me.com>
2020-09-12 00:50:44 +0200

    Better django

--- stock_data_project/stock_data_project/wsgi.py
+++ stock_data_project/stock_data_project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for stock_data project.
+WSGI config for stock_data_project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stock_data.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stock_data_project.settings')
 
 application = get_wsgi_application()
