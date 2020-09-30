25839605bf00d31e1a52faa67143400c6d0fc745
neriksen <nielseriksen@me.com>
2020-09-30 20:47:50 +0200

    prepping for dynamic weights

--- optimal_portfolio.py
+++ optimal_portfolio.py
@@ -4,6 +4,7 @@ import yfinance_loader as yfl
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+
 def get_returns(prices):
     cleaned = pd.DataFrame(prices).dropna()
     return cleaned.pct_change(fill_method="ffill").dropna()
@@ -27,18 +28,37 @@ def efficient_weights(cov_matrix, mean_returns):
     return weights/sum(weights)
 
 
-def yearly_returns(prices):
-    returns = prices.asfreq('D').ffill().asfreq('AS').pct_change().dropna(how='all').mean()
+def monthly_returns(prices):
+    returns = prices.asfreq('D').ffill().asfreq('M').pct_change().dropna(how='all').mean()
     if returns.isna().sum() > 0:
         return prices.iloc[-1, :]/prices.iloc[0, :]-1
     return returns
 
 
+#def dynamic_portfolio_value(endowment, weights, prices):
+#     if isinstance(weights, np.ndarray):
+#         weights = pd.DataFrame(np.full((prices.shape[0], prices.shape[1]), weights), index=prices.index)
+#         weights.columns = prices.columns
+#     print(weights)
+#     amounts = [(endowment * weights.iloc[0, :]).divide(prices.iloc[0, :])]
+#     value = [endowment]
+#     #initial_investment = endowment * weights
+#     for i in range(1, len(prices)):
+#         value.append((amounts[-1]*prices.iloc[i, :]).sum())
+#         amounts.append(weights.iloc[i, :]*value[-1]/prices.iloc[i, :])
+#
+#     return pd.DataFrame(value)
+
+
 def portfolio_value(endowment, weights, prices):
-    initial_investment = endowment * weights
-    amounts = initial_investment / prices.iloc[0, :]
-    value = pd.DataFrame(amounts * prices).sum(axis=1)
+    initial_investment = endowment*weights
+    amounts = initial_investment/prices.iloc[0, :]
+    value = pd.DataFrame(amounts*prices).sum(axis=1)
     return value
+
+
+def split_prices(prices, start_window = 0, len_window = 600):
+    return prices.iloc[start_window:start_window+len_window, :]
 
 
 #yfl.clean_raw_files()
@@ -47,21 +67,41 @@ def portfolio_value(endowment, weights, prices):
 #tickers = tickers[0].to_list()
 #tickers = [x.replace(' ', '-') + '.ST' for x in tickers]
 #tickers = ['VOO']
-tickers = ['JYSK.CO', 'DANSKE.CO', 'RILBA.CO', 'JUTBK.CO', 'NDA-DK.CO', 'VJBA.CO',
-           'SYDB.CO', 'SPNO.CO', 'MNBA.CO', 'FYNBK.CO', 'SKJE.CO', 'LOLB.CO', 'SALB.CO', 'DJUR.CO']
-#tickers = ['AAPL','TSLA', 'JPM', 'SNAP']
-prices = yfl.download_tickers(tickers, False, return_only=['Adj Close'], min_period='YTD').dropna()
+#tickers = ['JYSK.CO', 'DANSKE.CO', 'RILBA.CO', 'JUTBK.CO', 'NDA-DK.CO', 'VJBA.CO',
+#           'SYDB.CO', 'SPNO.CO', 'MNBA.CO', 'FYNBK.CO', 'SKJE.CO', 'LOLB.CO', 'SALB.CO', 'DJUR.CO']
+tickers = ['MBS', 'MGK', 'REMEDY.HE', 'PDX.ST']
+prices = yfl.download_tickers(tickers, False, return_only=['Adj Close'], min_period='2Y').dropna()
 
+start = 800
+weights = []
+rebalance_index = []
+# for i in range(start, len(prices)-start, 30):
+#     price = split_prices(prices, i, start)
+#     print(price)
+#     rebalance_index.append(price.index[-1])
+#     cov = get_covar_matrix(get_returns(price))
+#     efficient = efficient_weights(cov, yearly_returns(price)).tolist()
+#     weights.append(efficient)
+
+#eff_port_weights_raw = pd.DataFrame(weights, index=rebalance_index)
+
+#dynamic_eff_weights = pd.DataFrame(eff_port_weights_raw, index=prices.index).ffill(axis=0)\
+#    .fillna(1/len(eff_port_weights_raw.columns))
+#print(dynamic_eff_weights)
+#dynamic_eff_weights = yfl.create_stock_columns(dynamic_eff_weights, tickers, cols=['Adj Close'])
+
+#print(len(dynamic_eff_weights), len(prices))
 
 cov = get_covar_matrix(get_returns(prices))
 
 min_var = min_var_weights(cov)
-efficient = efficient_weights(cov, yearly_returns(prices))
+efficient = efficient_weights(cov, monthly_returns(prices))
 
 endowment = 100000
 
 portfolio_min = portfolio_value(endowment, min_var, prices)
 portfolio_eff = portfolio_value(endowment, efficient, prices)
+#portfolio_eff = portfolio_value(endowment, dynamic_eff_weights, prices)
 
 
 compare = 'SPY'
